8416f7f7e05b353a29d8eb242d36d4f936bafd08
osha7 <ogroetz@gmail.com>
2020-06-16 13:17:09 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,16 @@
+
 class Song < ActiveRecord::Base
+  
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  # def artist_name_and_title
+  #   artist_name + " - " + self.title
+  # end
+
+  #add this to song/index/html.erb:  <li><%= link_to song.artist_name_and_title, song_path(song) %></li><br>
+
 end
